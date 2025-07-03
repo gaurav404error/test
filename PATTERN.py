d7ae24a9aa137ca7e87496611a2e9d1bd651b29f
gaurav404error <singhvashistha7232gmail.com>
2025-07-03 13:27:08 +0530

    else elif

--- PATTERN.py
+++ PATTERN.py
@@ -1,6 +1,9 @@
-print('   ( . .)   / Hello')
-print('   |  | )  <  Junior')
-print('  (__ __)   \\  Coder!')
-print('  ------')
-
-m
+x=int("46:"))
+if x<0
+print('negative changed to zero')
+else x==0:
+print ('zero')
+elif x==1:
+print ('single')
+else:
+print('more')  
